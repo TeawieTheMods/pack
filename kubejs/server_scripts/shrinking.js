1c60a2b06437716af646d87a62eae54ed7d6f654
Skye <me@skye.vg>
2023-11-25 14:24:50 +0900

    chore: kubejs bump, resource pack

---
+++ kubejs/server_scripts/shrinking.js
@@ -0,0 +1,1 @@
+(()=>{ServerEvents.recipes(e=>{e.shaped("1x teawieks:scaler",["iei","igi","ibi"],{b:"minecraft:stone_button",e:"minecraft:ender_pearl",g:"minecraft:glass",i:"minecraft:iron_ingot"})});})();
