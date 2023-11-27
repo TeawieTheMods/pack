f25836feae06ea5de7efcfe16799f55fd38f7af0
Fnige <35159947+Fnige@users.noreply.github.com>
2023-11-27 19:22:24 +1100

    I did another (another) fucky wucky

---
+++ kubejs/server_scripts/fishing.js
@@ -0,0 +1,1 @@
+(()=>{LootJS.modifiers(a=>{a.addLootTableModifier("minecraft:gameplay/fishing/treasure").replaceLoot("minecraft:saddle","minecraft:heart_of_the_sea")});})();
---
+++ kubejs/server_scripts/recipe_removal.js
@@ -0,0 +1,1 @@
+(()=>{var e=["botania:mana_fluxfield","bhc:blade_of_vitality"];ServerEvents.recipes(t=>{for(let o in e)t.remove({output:`${e[o]}`})});})();
