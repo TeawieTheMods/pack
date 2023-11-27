643c0190cc783a4d1abe0b6e229d23b4250c09a8
Fnige <35159947+Fnige@users.noreply.github.com>
2023-11-27 19:08:32 +1100

    Removing fastbench/furnace due to https://github.com/illusivesoulworks/polymorph/issues/234

--- kubejs/client_scripts/tooltips.js
+++
@@ -1,1 +0,0 @@
-(()=>{var t=["botania:mana_fluxfield","bhc:blade_of_vitality"];ItemEvents.tooltip(e=>{for(let a in t)e.add(t[a],"\xA74This item has been disabled.")});})();
--- kubejs/server_scripts/fishing.js
+++
@@ -1,1 +0,0 @@
-(()=>{LootJS.modifiers(a=>{a.addLootTableModifier("minecraft:gameplay/fishing/treasure").replaceLoot("minecraft:saddle","minecraft:heart_of_the_sea")});})();
